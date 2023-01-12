29c1a169e23b0a966fbe213071e70776b67b933c
Niten619 <nitenlama46@gmail.com>
2023-01-12 15:14:19 +0545

    added timelog program

--- programs/stream-withdraw-timelog/src/lib.rs
+++ programs/stream-withdraw-timelog/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
-use anchor_lang::{system_program::{Transfer, transfer}};
+use anchor_lang::system_program::{Transfer as Sol_Transfer, transfer as sol_transfer};
+use anchor_spl::token::{Token, TokenAccount, Transfer as Token_Transfer, transfer as token_transfer};
 
 declare_id!("6MX8LNw7iRBabJwiHDfc58MJMs79gkd4tQXtPquHLCEd");
 
@@ -8,28 +9,28 @@ pub mod stream_withdraw_timelog {
     use super::*;
 
     pub fn stream_sol(ctx: Context<StreamSol>, amount: u64, start_time: u64) -> Result<()> {
-        ctx.accounts.timelog_account.start_time = start_time;
-        ctx.accounts.timelog_account.amount = amount;
-        ctx.accounts.timelog_account.receiver_account = *ctx.accounts.receiver_account.key;
+        ctx.accounts.timelog_account_sol.start_time = start_time;
+        ctx.accounts.timelog_account_sol.amount = amount;
+        ctx.accounts.timelog_account_sol.receiver_account = *ctx.accounts.receiver_account.key;
         
         let cpi_ctx_program = ctx.accounts.system_program.to_account_info();
-        let transfer_struct = Transfer{
+        let transfer_struct = Sol_Transfer{
             from: ctx.accounts.sender_account.to_account_info(),
             to: ctx.accounts.vault_account.to_account_info()
         };
         let cpi_ctx = CpiContext::new(cpi_ctx_program, transfer_struct);
-        transfer(cpi_ctx, amount)?;
+        sol_transfer(cpi_ctx, amount)?;
         Ok(())
     }
 
     pub fn withdraw_sol(ctx: Context<WithdrawSol>, amount: u64) -> Result<()> {   
         // check whether the account trying to withdraw the stream amount is authorized or not
-        if *ctx.accounts.receiver_account.key != ctx.accounts.timelog_account.receiver_account.key(){
+        if *ctx.accounts.receiver_account.key != ctx.accounts.timelog_account_sol.receiver_account.key(){
             msg!("Unauthorized Account!!!");
             return Err(error!(ErrorCode::InvalidProgramExecutable));
         }
         // check whether the amount trying to be withdrawn is more than the streamed amount
-        if amount > ctx.accounts.timelog_account.amount{
+        if amount > ctx.accounts.timelog_account_sol.amount{
             msg!("Amount limit Overreached!!!");
         }
         // get the latest timestamp
@@ -37,22 +38,74 @@ pub mod stream_withdraw_timelog {
         msg!("Time Now: {}", time_now);
 
         // check whether the latest timestamp is 24 hours later than the start_time recorded at streaming
-        // if time_now > ctx.accounts.timelog_account.start_time + (24 * 60 * 60){
+        // if time_now < ctx.accounts.timelog_account.start_time + (24 * 60 * 60){
             // msg!("24 hours has not passed from the time of stream!!!");
             // return ErrorCode::InvalidProgramExecutable;
         // }
-        if time_now < ctx.accounts.timelog_account.start_time + 4{
+        if time_now < ctx.accounts.timelog_account_sol.start_time + 4{
             msg!("4 sec has not passed from the time of stream!!!");
             return Err(error!(ErrorCode::InvalidProgramExecutable));
         }
 
         let cpi_ctx_program = ctx.accounts.system_program.to_account_info();
-        let transfer_struct = Transfer{
+        let transfer_struct = Sol_Transfer{
             from: ctx.accounts.vault_account.to_account_info(),
             to: ctx.accounts.receiver_account.to_account_info()
         };
         let cpi_ctx = CpiContext::new(cpi_ctx_program, transfer_struct);
-        transfer(cpi_ctx, amount)?;
+        sol_transfer(cpi_ctx, amount)?;
+        Ok(())
+    }
+
+
+    pub fn stream_spl(ctx: Context<StreamSpl>, amount: u64, start_time: u64) -> Result<()> {
+        ctx.accounts.timelog_account_spl.start_time = start_time;
+        ctx.accounts.timelog_account_spl.amount = amount;
+        ctx.accounts.timelog_account_spl.vault_ata = ctx.accounts.vault_ata.key();
+        ctx.accounts.timelog_account_spl.receiver_ata = ctx.accounts.receiver_ata.key();
+        
+        let cpi_ctx_program = ctx.accounts.token_program.to_account_info();
+        let transfer_struct = Token_Transfer{
+            from: ctx.accounts.sender_ata.to_account_info(),
+            to: ctx.accounts.vault_ata.to_account_info(),
+            authority: ctx.accounts.sender_account.to_account_info()
+        };
+        let cpi_ctx = CpiContext::new(cpi_ctx_program, transfer_struct);
+        token_transfer(cpi_ctx, amount)?;
+        Ok(())
+    }
+
+    pub fn withdraw_spl(ctx: Context<WithdrawSpl>, amount: u64) -> Result<()> {
+        // check whether the account trying to withdraw the stream amount is authorized or not
+        if ctx.accounts.receiver_ata.key() != ctx.accounts.timelog_account_spl.receiver_ata.key(){
+            msg!("Unauthorized Account!!!");
+            return Err(error!(ErrorCode::InvalidProgramExecutable));
+        }
+        // check whether the amount trying to be withdrawn is more than the streamed amount
+        if amount > ctx.accounts.timelog_account_spl.amount{
+            msg!("Amount limit Overreached!!!");
+        }
+        // get the latest timestamp
+        let time_now = Clock::get()?.unix_timestamp as u64;
+        msg!("Time Now: {}", time_now);
+        // check whether the latest timestamp is 24 hours later than the start_time recorded at streaming
+        // if time_now < ctx.accounts.timelog_account.start_time + (24 * 60 * 60){
+            // msg!("24 hours has not passed from the time of stream!!!");
+            // return ErrorCode::InvalidProgramExecutable;
+        // }
+        if time_now < ctx.accounts.timelog_account_spl.start_time + 4{
+            msg!("4 sec has not passed from the time of stream!!!");
+            return Err(error!(ErrorCode::InvalidProgramExecutable));
+        }
+        let cpi_ctx_program = ctx.accounts.token_program.to_account_info();
+        let transfer_struct = Token_Transfer{
+            from: ctx.accounts.vault_ata.to_account_info(),
+            to: ctx.accounts.receiver_ata.to_account_info(),
+            authority: ctx.accounts.vault_account.to_account_info()
+        };
+        let cpi_ctx = CpiContext::new(cpi_ctx_program, transfer_struct);
+        token_transfer(cpi_ctx, amount)?;
+
         Ok(())
     }
 }
@@ -60,8 +113,8 @@ pub mod stream_withdraw_timelog {
 #[derive(Accounts)]
 pub struct StreamSol<'info> {
     // This is how you create PDA with a desired seeds
-    #[account(init_if_needed, payer = sender_account, space = 8 + 8 + 8 + 32, seeds = [b"timelog", sender_account.key().as_ref()], bump)]
-    pub timelog_account: Account<'info, TimeLog>,
+    #[account(init_if_needed, payer = sender_account, space = 8 + 8 + 8 + 32, seeds = [b"timelog_sol", sender_account.key().as_ref()], bump)]
+    pub timelog_account_sol: Account<'info, TimeLogSol>,
     #[account(mut)]
     pub sender_account: Signer<'info>,
     /// CHECK:
@@ -76,8 +129,8 @@ pub struct StreamSol<'info> {
 #[derive(Accounts)]
 pub struct WithdrawSol<'info> {
     // This is how you reuse the already existing PDA with the help of seeds and bump 
-    #[account(mut, seeds = [b"timelog", sender_account.key().as_ref()], bump)]
-    pub timelog_account: Account<'info, TimeLog>,
+    #[account(mut, seeds = [b"timelog_sol", sender_account.key().as_ref()], bump)]
+    pub timelog_account_sol: Account<'info, TimeLogSol>,
     #[account(mut)]
     pub vault_account: Signer<'info>,
     /// CHECK:
@@ -90,9 +143,51 @@ pub struct WithdrawSol<'info> {
     
 }
 
+#[derive(Accounts)]
+pub struct StreamSpl<'info> {
+    // This is how you create PDA with a desired seeds
+    #[account(init_if_needed, payer = sender_account, space = 8 + 8 + 8 + 32 + 32, seeds = [b"timelog_spl", sender_account.key().as_ref()], bump)]
+    pub timelog_account_spl: Account<'info, TimeLogSpl>,
+    pub system_program: Program<'info, System>,
+    #[account(mut)]
+    pub sender_ata: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub vault_ata: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub receiver_ata: Account<'info, TokenAccount>,  // we need this to store it in PDA for verification checks in withdraw_spl function
+    #[account(mut)]
+    pub sender_account: Signer<'info>,
+    pub token_program: Program<'info, Token>
+    
+}
+
+#[derive(Accounts)]
+pub struct WithdrawSpl<'info> {
+    #[account(mut, seeds = [b"timelog_spl", sender_account.key().as_ref()], bump)]
+    pub timelog_account_spl:Account<'info, TimeLogSpl>,
+    /// CHECK:
+    #[account(mut)]
+    pub sender_account: AccountInfo<'info>,
+    #[account(mut)]
+    pub vault_ata: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub receiver_ata: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub vault_account: Signer<'info>,
+    pub token_program: Program<'info, Token>
+}
+
 #[account]
-pub struct TimeLog{
+pub struct TimeLogSol{
     start_time: u64,
     amount: u64,
     receiver_account: Pubkey,
+}
+
+#[account]
+pub struct TimeLogSpl{
+    start_time: u64,
+    amount: u64,
+    vault_ata: Pubkey,
+    receiver_ata: Pubkey,
 }
--- tests/stream-withdraw-timelog.ts
+++ tests/stream-withdraw-timelog.ts
@@ -1,13 +1,13 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { StreamWithdrawTimelog } from "../target/types/stream_withdraw_timelog";
-import { Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram } from "@solana/web3.js";
-import { TOKEN_PROGRAM_ID, MintLayout, getAssociatedTokenAddress, createAssociatedTokenAccountInstruction, createInitializeMintInstruction, createMintToInstruction, AccountLayout, closeAccount } from "@solana/spl-token";
+import { Keypair, LAMPORTS_PER_SOL, SystemProgram } from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID, MintLayout, getAssociatedTokenAddress, createAssociatedTokenAccountInstruction, createInitializeMintInstruction, createMintToInstruction, AccountLayout } from "@solana/spl-token";
 import { readFileSync } from "fs";
 import { BN } from "bn.js";
 import { findProgramAddressSync } from "@project-serum/anchor/dist/cjs/utils/pubkey";
 
-describe("stream-withdraw-timelog", () => {
+describe("stream-withdraw-timelog", async () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
@@ -25,30 +25,59 @@ describe("stream-withdraw-timelog", () => {
   console.log("Test Wallet 1 pubkey:", test_wallet_1_keypair.publicKey.toString())
   console.log("Test Wallet 2 pubkey:", test_wallet_2_keypair.publicKey.toString())
 
-  // get PDA address with the exact seeds value as used in the on-chain program
-  const [timelog_pda, _] = findProgramAddressSync(
-    [Buffer.from(anchor.utils.bytes.utf8.encode("timelog")),
+  // get timelog_sol PDA address with the exact seeds value as used in the on-chain program
+  const [timelog_sol_pda, _] = findProgramAddressSync(
+    [Buffer.from(anchor.utils.bytes.utf8.encode("timelog_sol")),
+    test_wallet_1_keypair.publicKey.toBuffer()],
+    program.programId
+  );
+  console.log("timelog_sol_pda:", timelog_sol_pda.toString())
+  // get timelog_spl PDA address with the exact seeds value as used in the on-chain program
+  const [timelog_spl_pda, __] = findProgramAddressSync(
+    [Buffer.from(anchor.utils.bytes.utf8.encode("timelog_spl")),
     test_wallet_1_keypair.publicKey.toBuffer()],
     program.programId
     );
-    console.log("timelog_pda:", timelog_pda.toString())
-    // generate a new keypair for the vault account
-    const vault_keypair = anchor.web3.Keypair.generate();
-    console.log("Vault pubkey:", vault_keypair.publicKey.toString())
+  console.log("timelog_spl_pda:", timelog_spl_pda.toString())
+  // generate a new keypair for the vault account
+  const vault_keypair = anchor.web3.Keypair.generate();
+  console.log("Vault pubkey:", vault_keypair.publicKey.toString())
+  // generate a new keypair for the mint account
+  const mint_keypair = anchor.web3.Keypair.generate();
+  console.log("Mint pubkey:", mint_keypair.publicKey.toString())
 
-    // a function to set timeout or sleep
-    const delay = ms => new Promise(res => setTimeout(res, ms));
+  // get the ATA address of the test wallet 1
+  const wallet_1_ata_address = await getAssociatedTokenAddress(
+    mint_keypair.publicKey,
+    test_wallet_1_keypair.publicKey
+  );
+  console.log("wallet_1_ata_address:", wallet_1_ata_address.toString())
+  // get the ATA address of the vault
+  const vault_ata_address = await getAssociatedTokenAddress(
+    mint_keypair.publicKey,
+    vault_keypair.publicKey
+  );
+  console.log("vault_ata_address:", vault_ata_address.toString())
+  // get the ATA address of the test wallet 2
+  const wallet_2_ata_address = await getAssociatedTokenAddress(
+    mint_keypair.publicKey,
+    test_wallet_2_keypair.publicKey
+  );
+  console.log("wallet_2_ata_address:", wallet_2_ata_address.toString())
+
+  // a function to set timeout or sleep
+  const delay = ms => new Promise(res => setTimeout(res, ms));
 
   it("Stream Sol Test!!!", async () => {
     let test_wallet_1_balance = await provider.connection.getBalance(test_wallet_1_keypair.publicKey)/LAMPORTS_PER_SOL;
     console.log("test_wallet_1_balance before:", test_wallet_1_balance)
 
-    // intruction for creating an account
+    // intruction for creating an account (vault account in this case)
     const vault_acc_inx = anchor.web3.SystemProgram.createAccount({
         fromPubkey: anchor_wallet.publicKey,
         newAccountPubkey: vault_keypair.publicKey,
-        lamports: 0,
-        space: AccountLayout.span,
+        lamports: 1_000_000_000,
+        space: 0,
         programId: SystemProgram.programId
     });
     // add intruction to a transaction and send it to the network
@@ -65,11 +94,12 @@ describe("stream-withdraw-timelog", () => {
     // get the timestamp at the time of stream
     const start_time = Math.floor(Date.now()/1000);
     console.log("start_time:", start_time)
+
     // make an rpc call to the on-chain program
     const tx_signature = await program.methods
     .streamSol(new BN(1_000_000_000), new BN(start_time))
     .accounts({
-        timelogAccount: timelog_pda,
+        timelogAccountSol: timelog_sol_pda,
         senderAccount: test_wallet_1_keypair.publicKey,
         vaultAccount: vault_keypair.publicKey,
         receiverAccount: test_wallet_2_keypair.publicKey,
@@ -77,7 +107,8 @@ describe("stream-withdraw-timelog", () => {
     })
     .signers([test_wallet_1_keypair])
     .rpc();
-    console.log("Your transaction signature", tx_signature);
+    console.log("Your transaction signature", tx_signature)
+
     vault_account_balance = await provider.connection.getBalance(vault_keypair.publicKey)/LAMPORTS_PER_SOL;
     console.log("vault_account_balance after:", vault_account_balance)
     test_wallet_1_balance = await provider.connection.getBalance(test_wallet_1_keypair.publicKey)/LAMPORTS_PER_SOL;
@@ -99,7 +130,7 @@ describe("stream-withdraw-timelog", () => {
     const tx_signature = await program.methods
     .withdrawSol(new BN(1_000_000_000))
     .accounts({
-        timelogAccount: timelog_pda,
+        timelogAccountSol: timelog_sol_pda,
         vaultAccount: vault_keypair.publicKey,
         receiverAccount: test_wallet_2_keypair.publicKey,
         senderAccount: test_wallet_1_keypair.publicKey,
@@ -107,11 +138,136 @@ describe("stream-withdraw-timelog", () => {
     })
     .signers([vault_keypair])
     .rpc();
-    console.log("Your transaction signature", tx_signature);
+    console.log("Your transaction signature", tx_signature)
+
     vault_account_balance = await provider.connection.getBalance(vault_keypair.publicKey)/LAMPORTS_PER_SOL;
     console.log("vault_account_balance after:", vault_account_balance)
     test_wallet_2_balance = await provider.connection.getBalance(test_wallet_2_keypair.publicKey)/LAMPORTS_PER_SOL;
     console.log("test_wallet_2_balance after:", test_wallet_2_balance)
     
+  });
+
+  it("Stream Spl Test!!!", async () => {
+    // instruction for creating an account (mint account in this case)
+    const mint_acc_inx = anchor.web3.SystemProgram.createAccount({
+      fromPubkey: anchor_wallet.publicKey,
+      newAccountPubkey: mint_keypair.publicKey,
+      lamports: 1_000_000_000,
+      space: MintLayout.span,
+      programId: TOKEN_PROGRAM_ID
+    });
+    // instruction for initializing the mint account
+    const mint_acc_init_inx = createInitializeMintInstruction(
+      mint_keypair.publicKey,
+      9,
+      test_wallet_1_keypair.publicKey,
+      test_wallet_1_keypair.publicKey
+    )
+
+    // get the timestamp at the time of stream
+    const start_time = Math.floor(Date.now()/1000);
+    console.log("start_time:", start_time)
+
+    // instruction for creating associated token account (sender ata)
+    const sender_ata_inx = createAssociatedTokenAccountInstruction(
+      test_wallet_1_keypair.publicKey,
+      wallet_1_ata_address,
+      test_wallet_1_keypair.publicKey,
+      mint_keypair.publicKey
+    );
+    // instruction for creating associated token account  (vault ata)
+    const vault_ata_inx = createAssociatedTokenAccountInstruction(
+      vault_keypair.publicKey,
+      vault_ata_address,
+      vault_keypair.publicKey,
+      mint_keypair.publicKey
+    );
+    // instruction for creating associated token account (receiver ata)
+    const receiver_ata_inx = createAssociatedTokenAccountInstruction(
+      test_wallet_2_keypair.publicKey,
+      wallet_2_ata_address,
+      test_wallet_2_keypair.publicKey,
+      mint_keypair.publicKey
+    );
+    // instruction for minting 100 tokens into test wallet 1's ata
+    console.log("Forming instruction for minting 100 tokens into", wallet_1_ata_address.toString())
+    const mint_token_inx = createMintToInstruction(
+        mint_keypair.publicKey,
+        wallet_1_ata_address,
+        test_wallet_1_keypair.publicKey,
+        100_000_000_000
+    );
+    // add all 3 instructions to a transaction
+    const tnx = new anchor.web3.Transaction().add(
+      mint_acc_inx,       // instruction for creating mint account
+      mint_acc_init_inx,  // instruction for initializing the mint account
+      sender_ata_inx,     // instruction for creating sender ata
+      vault_ata_inx,      // instruction for creating vault ata
+      receiver_ata_inx,   // instruction for creating receiver ata
+      mint_token_inx      // instruction for minting tokens
+    );
+    // send the transaction to the network
+    const tnx_sig = await anchor.AnchorProvider.env().sendAndConfirm(
+      tnx,
+      [test_wallet_1_keypair, test_wallet_2_keypair, vault_keypair, mint_keypair],
+      {preflightCommitment: "confirmed"}
+      );
+    console.log("tnx signature:", tnx_sig)
+
+    let test_wallet_1_token_balance = await provider.connection.getTokenAccountBalance(wallet_1_ata_address);
+    console.log("test_wallet_1_token_balance before:", test_wallet_1_token_balance)
+    let vault_ata_balance = await provider.connection.getTokenAccountBalance(vault_ata_address);
+    console.log("vault_ata_balance before:", vault_ata_balance)
+
+    // make an rpc call to the on-chain program
+    const tx = await program.methods.streamSpl(
+      new BN(10_000_000_000),
+      new BN(start_time)
+    ).accounts({
+      senderAccount: test_wallet_1_keypair.publicKey,
+      systemProgram: SystemProgram.programId,
+      timelogAccountSpl: timelog_spl_pda,
+      senderAta: wallet_1_ata_address,
+      vaultAta: vault_ata_address,
+      receiverAta: wallet_2_ata_address,
+      tokenProgram: TOKEN_PROGRAM_ID
+    }).signers([test_wallet_1_keypair])
+    .rpc();
+    console.log("Your transaction signature", tx)
+
+    test_wallet_1_token_balance = await provider.connection.getTokenAccountBalance(wallet_1_ata_address);
+    console.log("test_wallet_1_token_balance after:", test_wallet_1_token_balance)
+    vault_ata_balance = await provider.connection.getTokenAccountBalance(vault_ata_address);
+    console.log("vault_ata_balance after:", vault_ata_balance)
+  });
+
+  it("Withdraw Spl Test!!!", async () => {
+    console.log("Waiting for 5 secs...")
+    await delay(5000);
+    console.log("5 secs Over")
+
+    let vault_ata_balance = await provider.connection.getTokenAccountBalance(vault_ata_address);
+    console.log("vault_ata_balance before:", vault_ata_balance)
+    let test_wallet_2_token_balance = await provider.connection.getTokenAccountBalance(wallet_2_ata_address);
+    console.log("test_wallet_2_token_balance before:", test_wallet_2_token_balance)
+
+    // make an rpc call to the on-chain program
+    const tx = await program.methods.withdrawSpl(
+      new BN(10_000_000_000)
+    ).accounts({
+      timelogAccountSpl: timelog_spl_pda,
+      senderAccount: test_wallet_1_keypair.publicKey,
+      vaultAta: vault_ata_address,
+      receiverAta: wallet_2_ata_address,
+      vaultAccount: vault_keypair.publicKey,
+      tokenProgram: TOKEN_PROGRAM_ID
+    }).signers([vault_keypair])
+    .rpc();
+    console.log("Your transaction signature", tx)
+
+    vault_ata_balance = await provider.connection.getTokenAccountBalance(vault_ata_address);
+    console.log("vault_ata_balance after:", vault_ata_balance)
+    test_wallet_2_token_balance = await provider.connection.getTokenAccountBalance(wallet_2_ata_address);
+    console.log("test_wallet_2_token_balance after:", test_wallet_2_token_balance)
   });
 });
